8a1e90c7b87a2934904ff0763adf2377c02e00a8
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-08-06 10:32:55 +0530

    Create index.js

---
+++ Episode-3/index.js
@@ -0,0 +1,29 @@
+const { Collection, Client } = require('discord.js');
+const client = new Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+    ],
+});
+module.exports = client;
+
+client.commands = new Collection()
+client.config = require('./config.json')
+client.prefix = client.config.prefix
+client.aliases = new Collection()
+
+require('./handler')(client);
+
+client.login(client.config.token)
