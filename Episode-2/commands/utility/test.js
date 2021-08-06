8002d10f0cd8fd9c5f66b30e95ec0a4c3bad7348
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-08-06 10:31:41 +0530

    Making a Command and Event Handler

---
+++ Episode-2/commands/utility/test.js
@@ -0,0 +1,15 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'test',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async (client, message, args) => {
+        const text = args.slice(0).join(' ')
+        if (!text) return message.channel.send({ content: 'Tell me something' })
+        message.channel.send({ content: `The text you told me is ${text}` })
+    }
+} // Now lets test this command
